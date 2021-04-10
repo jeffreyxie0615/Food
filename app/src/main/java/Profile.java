2fb0542f35f8daa20da740fcc85fd7d7a831a69d
SaketG1 <65042050+SaketG1@users.noreply.github.com>
2021-04-10 16:32:55 -0500

    TONG WEI

--- app/src/main/java/Profile.java
+++ app/src/main/java/Profile.java
@@ -1,2 +1,15 @@
+enum Cuisine{
+    MEXICAN, ITALIAN, ASIAN, AMERICAN, MEDITERRANEAN;
+}
+
 public class Profile {
+    private String userName;
+    private String password;
+    private Cuisine cuisine;
+
+    public Profile(String password, String userName){
+        this.userName = userName;
+        this.password = password;
+    }
+
 }
