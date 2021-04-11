a1ccd7166876020d2ddbb84237e4a2e05f839bbe
Jeffrey <jeffreyxie0615@gmail.com>
2021-04-11 17:20:06 -0500

    fixed

--- app/src/main/java/com/example/food/FourthFragment.java
+++ app/src/main/java/com/example/food/FourthFragment.java
@@ -1,5 +1,7 @@
 package com.example.food;
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.StrictMode;
 import android.view.LayoutInflater;
@@ -7,15 +9,16 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
+import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
 import com.github.scribejava.core.builder.ServiceBuilder;
 import com.github.scribejava.core.oauth.OAuth10aService;
-import com.yelp.clientlib.connection.YelpAPI;
-import com.yelp.clientlib.connection.YelpAPIFactory;
-import com.yelp.clientlib.entities.Business;
-import com.yelp.clientlib.entities.SearchResponse;
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.yelp.fusion.client.connection.*;
+import com.yelp.fusion.client.models.SearchResponse;
+
 
 import java.io.IOException;
 import java.util.*;
@@ -31,8 +34,8 @@ import retrofit2.Response;
 public class FourthFragment extends Fragment {
 
     private final static String clientID = "G9ds1N_jO7s-9TTA2Q-seQ";
-    private final static String APIKey = "vHwW63JHeiitHViZ_g6Y8JQUw-P-enPX4zi3BGVZkBCa05IQs6UPcXX3QD" +
-            "yCvuQib2yLUULmud0UuHruGTcx2Rcj5fxD2eCPGemezWFNpdnAda9puXgoX_9SA0NzYHYx";
+    private final static String APIKey = "vHwW63JHeiitHViZ_g6Y8JQUw-P-enPX4zi3BGVZkBCa05IQs6UPcXX3QDyCvuQib2yLUULmud0UuHruGTcx2Rcj5fxD2eCPGemezWFNpdnAda9puXgoX_9SA0NzYHYx";
+
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -55,21 +58,32 @@ public class FourthFragment extends Fragment {
             transactionRealm.insertOrUpdate(newPerson);
         });
         Profile newPerson = backgroundThreadRealm.where(Profile.class).contains("username", SecondFragment.currUser).findAll().get(0);
-        YelpAPIFactory apiFactory = new YelpAPIFactory(clientID, APIKey, "", "");
-        YelpAPI yelpAPI = apiFactory.createAPI();
-        Map<String, String> params = new HashMap<>();
-        params.put("term", newPerson.cuisine);
+        YelpFusionApiFactory apiFactory = new YelpFusionApiFactory();
         StrictMode.ThreadPolicy policy = new StrictMode.ThreadPolicy.Builder().permitAll().build();
         StrictMode.setThreadPolicy(policy);
         try {
-            Call<SearchResponse> call = yelpAPI.search("Austin", params);
-            SearchResponse searchResponse = call.execute().body();
-            int totalNumberOfResult = searchResponse.total();  // 3
-            ArrayList<Business> businesses = searchResponse.businesses();
-            String businessName = businesses.get(0).name();
-            Double rating = businesses.get(0).rating();
-            System.out.println(businessName);
+            YelpFusionApi yelpFusionApi = apiFactory.createAPI(APIKey);
+            Map<String, String> params = new HashMap<>();
+            params.put("term", newPerson.cuisine);
+            if (ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(getContext(), Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+                // TODO: Consider calling
+                //    ActivityCompat#requestPermissions
+                // here to request the missing permissions, and then overriding
+                //   public void onRequestPermissionsResult(int requestCode, String[] permissions,
+                //                                          int[] grantResults)
+                // to handle the case where the user grants the permission. See the documentation
+                // for ActivityCompat#requestPermissions for more details.
+
+            }
+
+
+            params.put("latitude", MainActivity.latitude);
+            params.put("longitude", MainActivity.longitude);
+            Call<SearchResponse> call = yelpFusionApi.getBusinessSearch(params);
+            Response<SearchResponse> response = call.execute();
+            System.out.println(response.body().getBusinesses().get(0).getName());
         } catch (IOException e) {
+            System.out.println("here");
             System.out.println(e);
         }
 
--- app/src/main/java/com/example/food/MainActivity.java
+++ app/src/main/java/com/example/food/MainActivity.java
@@ -1,27 +1,64 @@
 package com.example.food;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationManager;
 import android.os.Bundle;
 
 import com.google.android.material.floatingactionbutton.FloatingActionButton;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
 
+import android.os.Looper;
+import android.provider.Settings;
 import android.view.View;
 
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
+import android.widget.Toast;
 
+import com.google.android.gms.location.FusedLocationProviderClient;
+import com.google.android.gms.location.LocationCallback;
+import com.google.android.gms.location.LocationRequest;
+import com.google.android.gms.location.LocationResult;
+import com.google.android.gms.location.LocationServices;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
 public class MainActivity extends AppCompatActivity {
     private static final String DEBUG_TAG = "MOVE";
+    // initializing
+    // FusedLocationProviderClient
+    // object
+    LocationManager locationManager;
+    public static String latitude, longitude;
+    public static FusedLocationProviderClient mFusedLocationClient;
+
+    // Initializing other items
+    // from layout file
+    TextView latitudeTextView, longitTextView;
+    int PERMISSION_ID = 44;
+    private static final int REQUEST_LOCATION = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-
+        ActivityCompat.requestPermissions( this,
+                new String[] {Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        getLocation();
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -54,6 +91,39 @@ public class MainActivity extends AppCompatActivity {
 
         return super.onOptionsItemSelected(item);
     }
-
+    private void OnGPS() {
+        final AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Enable GPS").setCancelable(false).setPositiveButton("Yes", new  DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                startActivity(new Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS));
+            }
+        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        final AlertDialog alertDialog = builder.create();
+        alertDialog.show();
+    }
+    private void getLocation() {
+        if (ActivityCompat.checkSelfPermission(
+                MainActivity.this,Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(
+                MainActivity.this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, REQUEST_LOCATION);
+        } else {
+            Location locationGPS = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (locationGPS != null) {
+                double lat = locationGPS.getLatitude();
+                double longi = locationGPS.getLongitude();
+                latitude = String.valueOf(lat);
+                longitude = String.valueOf(longi);
+            } else {
+                latitude = "40.1020";
+                longitude="-88.2272";
+            }
+        }
+    }
 
 }
