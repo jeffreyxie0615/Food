c8cf814cd058a4e4ddf754c68617b154623604c8
Jeffrey <jeffreyxie0615@gmail.com>
2021-04-11 14:35:58 -0500

    fix

--- app/src/main/java/com/example/food/FourthFragment.java
+++ app/src/main/java/com/example/food/FourthFragment.java
@@ -9,12 +9,17 @@ import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.fragment.NavHostFragment;
 
+import java.net.HttpURLConnection;
+import java.net.URL;
 import java.Algo;
 import java.Profile;
 
 import io.realm.*;
 public class FourthFragment extends Fragment {
 
+    private final static String clientID = "G9ds1N_jO7s-9TTA2Q-seQ";
+    private final static String APIKey = "vHwW63JHeiitHViZ_g6Y8JQUw-P-enPX4zi3BGVZkBCa05IQs6UPcXX3QD" +
+            "yCvuQib2yLUULmud0UuHruGTcx2Rcj5fxD2eCPGemezWFNpdnAda9puXgoX_9SA0NzYHYx";
     @Override
     public View onCreateView(
             LayoutInflater inflater, ViewGroup container,
@@ -36,5 +41,16 @@ public class FourthFragment extends Fragment {
             newPerson.cuisine = work.getCuisine();
             transactionRealm.insertOrUpdate(newPerson);
         });
+        try {
+            URL url = new URL("https://api.yelp.com/oauth2/token?grant_type=client_credentials&client_secret=vHwW63JHeiitHViZ_g6Y8JQUw-P-enPX4zi3BGVZkBCa05IQs6UPcXX3QD\" +\n" +
+                    "            \"yCvuQib2yLUULmud0UuHruGTcx2Rcj5fxD2eCPGemezWFNpdnAda9puXgoX_9SA0N" +
+                    "zYHYx&client_id=G9ds1N_jO7s-9TTA2Q-seQ");
+            HttpURLConnection con = (HttpURLConnection) url.openConnection();
+            con.setRequestMethod("GET");
+            Object yelp = con.getContent();
+            System.out.println(yelp);
+            System.out.println("yelp");
+        }catch (Exception e){}
+
     }
 }
